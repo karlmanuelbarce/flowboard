aa75a2525c6962ec69eec7723ffea4924b529939
Karl Barce <karlbarce@ITXs-MacBook-Pro.local>
2026-05-18 20:19:25 +0800

    fix: update Prisma client provider and import path

--- api/src/middleware/db.ts
+++ api/src/middleware/db.ts
@@ -1,6 +1,6 @@
 // src/db.ts
 import 'dotenv/config';
-import { PrismaClient } from '../generated/prisma/client.ts';
+import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const url = process.env['DATABASE_URL'];
